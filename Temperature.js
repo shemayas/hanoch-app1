1fecdac5d53c16604d669c1d2ce28f08d72bc11b
Hanoch Spero <speroim@gmail.com>
2024-05-08 23:05:59 +0300

    initial commint

---
+++ Temperature.js
@@ -0,0 +1,11 @@
+const textBox = document.getElementById("textBox");
+const ferenhigetTO = document.getElementById("ferenhigetTO");
+const celsiosTO = document.getElementById("celsiosTO");
+const answer = document.getElementById("answer");
+let temp;
+console.log("hi");
+
+function convort(){
+    answer.textContent = "ferenhigetTO";
+   
+}
